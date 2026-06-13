a91f56548ce5db72770e02d064b10c007a093092
Anushya <anushyaswamy@gmail.com>
2026-06-13 08:13:42 +0530

    Added EmployeesController and Swagger support

---
+++ EmployeeManagement.API/Controllers/EmployeesController.cs
@@ -0,0 +1,35 @@
+﻿using EmployeeManagement.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EmployeesController : ControllerBase
+{
+    private readonly IEmployeeRepository _employeeRepository;
+
+    public EmployeesController(IEmployeeRepository employeeRepository)
+    {
+        _employeeRepository = employeeRepository;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllEmployees()
+    {
+        var employees = await _employeeRepository.GetAllAsync();
+
+        return Ok(employees);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetEmployeeById(int id)
+    {
+        var employee = await _employeeRepository.GetByIdAsync(id);
+
+        if (employee == null)
+            return NotFound();
+
+        return Ok(employee);
+    }
+}
--- EmployeeManagement.API/Program.cs
+++ EmployeeManagement.API/Program.cs
@@ -1,15 +1,22 @@
 using EmployeeManagement.Infrastructure.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddInfrastructure(builder.Configuration);
 
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.MapControllers();
 
 app.Run();
